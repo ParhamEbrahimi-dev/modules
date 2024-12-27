8faa2c9c45a23a4411db0e2295e3315e9f913ae8
Parham Ebrahimi <ParhamEbrahimi.dev@gmail.com>
2024-12-27 18:17:10 +0330

    Add Segment with ng-content select

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -6,13 +6,15 @@ import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceHolderComponent } from './place-holder/place-holder.component';
 import { RepeatTemplateDirective } from './repeat-template.directive';
 import { SharedModule } from '../shared/shared.module';
+import { SegmentComponent } from './segment/segment.component';
 
 
 @NgModule({
   declarations: [
     ElementsHomeComponent,
     PlaceHolderComponent,
-    RepeatTemplateDirective
+    RepeatTemplateDirective,
+    SegmentComponent
   ],
   imports: [
     CommonModule,
