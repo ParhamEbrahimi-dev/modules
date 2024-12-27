571d4523c413224992c08b8052e65554fc83a503
Parham Ebrahimi <ParhamEbrahimi.dev@gmail.com>
2024-12-27 12:26:10 +0330

    Add collections component

---
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collections-home',
+  standalone: false,
+  
+  templateUrl: './collections-home.component.html',
+  styleUrl: './collections-home.component.css'
+})
+export class CollectionsHomeComponent {
+
+}
