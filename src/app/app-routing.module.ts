3ec000bab83e725b49c121ec80b4fd0d83847c3a
Parham Ebrahimi <ParhamEbrahimi.dev@gmail.com>
2024-12-27 15:17:05 +0330

    add lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
+  {path : "elements",loadChildren:()=>import("./elements/elements.module").then(m=>m.ElementsModule)},
+  {path : "collections",loadChildren: () => import("./collections/collections.module").then(m=>m.CollectionsModule)},
   {path:"home",component : HomeComponent},
-
   {path:"", redirectTo:"/home", pathMatch:'full'},
   {path :"not-found",component : NotFoundComponent},
   {path : "**", redirectTo : "/not-found"}
