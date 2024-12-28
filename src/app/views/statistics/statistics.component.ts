a0a5756be2a9fdc8158e5dae73ad5ab15041742e
Parham Ebrahimi <ParhamEbrahimi.dev@gmail.com>
2024-12-28 14:23:23 +0330

    add Item List Views

--- src/app/views/statistics/statistics.component.ts
+++ src/app/views/statistics/statistics.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-statistics',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './statistics.component.css'
 })
 export class StatisticsComponent {
-
+  @Input() data:any[]=[];
 }
--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './views-home.component.css'
 })
 export class ViewsHomeComponent {
+  stats=[
+    {value:33,label:"# of Users"},
+    {value:22,label:"# Likes"},
+    {value:9,label:"# Comments"},
+  ]
+  items=[
+    {
+      image:"/public/assets/images/couch.jpg",
+      header:"couch",
+      title:"white couch",
+      description:"this is a goods choise for everyone"
 
+    },
+    {
+      image:"/public/assets/images/dresser.jpg",
+      header:"dresser",
+      title:"white dresser",
+      description:"this is a goods choise for young People"
+    }
+  ]
 }
